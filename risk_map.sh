935b8fb5b891b4b04b7dc88fbbcf61f4287cfb0c
Aziz Alghunaim <azizkag@mit.edu>
2012-08-09 00:48:02 +0300

    final risk map

--- risk_map.sh
+++ risk_map.sh
@@ -7,67 +7,70 @@ convert -size 8776x7612 xc:"#ff4e4e" red2.png
 convert -size 8776x7612 xc:"#d80000" red3.png
 convert -size 8776x7612 xc:"#890000" red4.png
 
+#Unify the coordinate system between the landsat imagery and elevation
+
+gdalwarp -t_srs '+proj=UTM +zone=48 +datum=WGS84' elv.tif elv1.tif
 
 
-#convert tiff to png (to allow work with alpha channel)
-convert veg.tif veg.png
-convert urb.tif urb.png
+#Resize the georeferenced elevation to match the region of interest
+
+gdalwarp -te 620459.250 -748139.250 870575.250 -531197.250 -ts 8776 7612 elv1.tif elv.tif
 
 
 #1- isolate the veg, and urb
 
-convert urb.png -matte \( +clone -fuzz 15% -transparent "#b04e9a" \) -compose DstOut -composite urb_iso.png
+convert urb.tif -matte \( +clone -fuzz 15% -transparent "#b04e9a" \) -compose DstOut -composite urb_iso.tif
 
 
-convert veg.png -matte \( +clone -fuzz 15% -transparent "#874864" \) -compose DstOut -composite veg_iso.png
+convert veg.tif -matte \( +clone -fuzz 15% -transparent "#874864" \) -compose DstOut -composite veg_iso.tif
 
 #2- Obtain an elevation map that matches veg, and urb 
 
-convert urb_iso.png -alpha extract urb_alpha.png
-convert veg_iso.png -alpha extract veg_alpha.png
+convert urb_iso.tif -alpha extract urb_alpha.tif
+convert veg_iso.tif -alpha extract veg_alpha.tif
 
-convert elv.png veg_alpha.png -alpha off -compose CopyOpacity -composite veg_elv.png
-convert elv.png urb_alpha.png -alpha off -compose CopyOpacity -composite urb_elv.png
+convert elv.tif veg_alpha.tif -alpha off -compose CopyOpacity -composite veg_elv.tif
+convert elv.tif urb_alpha.tif -alpha off -compose CopyOpacity -composite urb_elv.tif
 
 #3- Divide the two maps 
-composite veg_iso.png -compose Divide veg_elv.png Divide_veg.png
-composite urb_iso.png -compose Divide urb_elv.png Divide_urb.png
+composite veg_iso.tif -compose Divide veg_elv.tif Divide_veg.tif
+composite urb_iso.tif -compose Divide urb_elv.tif Divide_urb.tif
 
 
 #4- isolate HL veg and HL urb
 
-convert Divide_veg.png -matte \( +clone -fuzz 25% -transparent "#ff71ab" \) -compose DstOut -composite veg_HL.png
+convert Divide_veg.tif -matte \( +clone -fuzz 25% -transparent "#ff71ab" \) -compose DstOut -composite veg_HL.tif
 
 ##the color below might need to be changed ###
-convert Divide_urb.png -matte \( +clone -fuzz 25% -transparent "#ff71ab" \) -compose DstOut -composite urb_HL.png
+convert Divide_urb.tif -matte \( +clone -fuzz 25% -transparent "#ff71ab" \) -compose DstOut -composite urb_HL.tif
 
 
-#5- use the HL veg Mask to generate LL veg Mask (the same of urb)
-convert veg_HL.png -alpha extract veg_HL_alpha.png
-convert veg_HL_alpha.png -negate veg_HL_alpha_n.png
-composite veg_HL_alpha_n.png -compose Multiply veg_alpha.png veg_LL_alpha.png
+#5- use the HL veg Mask to generate LL veg Mask (the same for urb)
+convert veg_HL.tif -alpha extract veg_HL_alpha.tif
+convert veg_HL_alpha.tif -negate veg_HL_alpha_n.tif
+composite veg_HL_alpha_n.tif -compose Multiply veg_alpha.tif veg_LL_alpha.tif
 
-convert urb_HL.png -alpha extract urb_HL_alpha.png
-convert urb_HL_alpha.png -negate urb_HL_alpha_n.png
-composite urb_HL_alpha_n.png -compose Multiply urb_alpha.png urb_LL_alpha.png
+convert urb_HL.tif -alpha extract urb_HL_alpha.tif
+convert urb_HL_alpha.tif -negate urb_HL_alpha_n.tif
+composite urb_HL_alpha_n.tif -compose Multiply urb_alpha.tif urb_LL_alpha.tif
 
 
 #7- color each level with a color corresponding to risk level
 
-convert blue.png veg_HL_alpha.png -alpha off -compose CopyOpacity -composite veg_HL_blue.png
+convert red1.png veg_HL_alpha.tif -alpha off -compose CopyOpacity -composite veg_HL_red1.tif
 
-convert red.png veg_LL_alpha.png -alpha off -compose CopyOpacity -composite veg_LL_red.png
+convert red2.png veg_LL_alpha.tif -alpha off -compose CopyOpacity -composite veg_LL_red2.tif
 
 
-convert green.png urb_HL_alpha.png -alpha off -compose CopyOpacity -composite urb_HL_green.png
+convert red3.png urb_HL_alpha.tif -alpha off -compose CopyOpacity -composite urb_HL_red3.tif
 
-convert yellow.png urb_LL_alpha.png -alpha off -compose CopyOpacity -composite urb_LL_yellow.png
+convert red4.png urb_LL_alpha.tif -alpha off -compose CopyOpacity -composite urb_LL_red4.tif
 
 #8- add the layers together to create the color coded risk map
-composite veg_HL_blue.png -compose Plus veg_LL_red.png veg_LLPlusHL.png
-composite urb_HL_green.png -compose Plus urb_LL_yellow.png urb_LLPlusHL.png
+composite veg_HL_red1.tif -compose Plus veg_LL_red2.tif veg_LLPlusHL.tif
+composite urb_HL_red3.tif -compose Plus urb_LL_red4.tif urb_LLPlusHL.tif
 
-composite urb_LLPlusHL.png -compose Minus veg_LLPlusHL.png risk_veg_urb.png
+composite urb_LLPlusHL.tif -compose Minus veg_LLPlusHL.tif risk-_veg_urb.tif
 
 
 
